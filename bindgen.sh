318ed5cf22d240c2a300a67ee08960a4927fb023
Marc Nijdam <marc_nijdam@mac.com>
2020-10-20 16:14:20 -0500

    Fix up bindings some more

---
+++ bindgen.sh
@@ -0,0 +1,1 @@
+bindgen src/bindings.h -o src/bindings.rs --whitelist-function '^lfc_.*' --whitelist-var '^lfc_.*' -- -I vendor/include
---
+++ src/bindings.h
@@ -0,0 +1,3 @@
+#include "lfc/datagram.h"
+#include "lfc/fingerprint.h"
+#include "lfc/lfc.h"
